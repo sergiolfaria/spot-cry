510b1cf7df5ca8f0a22e6d6c8e2eb9776acdfd86
sergiolfaria <srgion@yahoo.com.br>
2023-11-12 18:35:10 -0300

    Metodo de Post adicionado para songs

---
+++ src/components/FormPostSongs/PostSongs.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import {postSongsToData}  from '../../services/addNewSongs';
+
+
+const SongForm = () => {
+  const [songData, setSongData] = useState({
+    title: '',
+    artist: '',
+    url: ''
+  });
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      await postSongsToData(songData);
+      setSongData({
+        title: '',
+        artist: '',
+        url: ''
+      });
+
+      alert('Música postada com sucesso!');
+    } catch (error) {
+      console.error('Erro ao postar a música:', error);
+      alert('Erro ao postar a música. Por favor, tente novamente.');
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setSongData({
+      ...songData,
+      [name]: value,
+    });
+  };
+
+  return (
+    <div>
+      <h2>Postar Nova Música</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Título:
+          <input
+            type="text"
+            name="title"
+            value={songData.title}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Artista:
+          <input
+            type="text"
+            name="artist"
+            value={songData.artist}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        
+        <label>
+          URL:
+          <input
+            type="text"
+            name="url"
+            value={songData.url}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Postar Música</button>
+      </form>
+    </div>
+  );
+};
+
+export default SongForm;
--- src/pages/FeedPage.js
+++ src/pages/FeedPage.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import { getPlaylistsFromUser } from "../services/playlist";
+import { getMusicsFromData } from "../services/getMusicData";
+import SongForm from './../components/FormPostSongs/PostSongs';
 
 export function FeedPage() {
   const [playlists, setPlaylists] = useState([]);
+  const [songs, setsongs] = useState([]);
 
   useEffect(() => {
     fetchPlaylists();
-  }, []);
+    fetchMusics();
+  }, [songs,playlists]);
 
   const fetchPlaylists = async () => {
     try {
@@ -15,6 +18,14 @@ export function FeedPage() {
       setPlaylists(response.data.playlists);
     } catch (error) {
       console.error("Erro ao buscar playlists:", error);
+    }
+  };
+  const fetchMusics = async () => {
+    try {
+      const response = await getMusicsFromData();
+      setsongs(response.data.songs);
+    } catch (error) {
+      console.error("Erro ao buscar musicas:", error);
     }
   };
 
@@ -27,6 +38,14 @@ export function FeedPage() {
             <li key={playlist._id}>{index+1} {playlist._name} </li>
           ))}
       </ul>
+      <h1>all songs</h1>
+      <ul>
+        {Array.isArray(songs) &&
+          songs.map((songs,index) => (
+            <li key={songs.id}>{index+1} {songs.title} </li>
+          ))}
+      </ul>
+      <SongForm />
     </div>
   );
 }
